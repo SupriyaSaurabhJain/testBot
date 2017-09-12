c93cc76a8c174b291c9f005c5ea8129672ae6ac3
saiba09 <saiba_sheikh@persistent.co.in>
2017-09-12 15:31:50 +0530

    Update readFromExcel.java
    escapeSequence added

--- src/main/java/com/ey/db/readFromExcel.java
+++ src/main/java/com/ey/db/readFromExcel.java
@@ -131,9 +131,9 @@ public class readFromExcel {
 
 				for (String state : stateLawMap.keySet()) {
 
-
+					String escapeSequence = "\\\\" ;
 					log.info("state : " + state);
-					String lawDescription = stateLawMap.get(state).replaceAll("\'", "\\\'").replaceAll("\"", "\\\""); //.replaceAll("\n", "/\n").replaceAll("\t", "/\t");
+					String lawDescription = stateLawMap.get(state).replaceAll("\'", escapeSequence+"\'").replaceAll("\"", escapeSequence+"\""); //.replaceAll("\n", "/\n").replaceAll("\t", "/\t");
 					if (state.equalsIgnoreCase("FEDERAL")) {
 						log.info("insert into Law_Description(law_description1,country_id,state_id,sub_topic_id) Values('"+lawDescription+"','"+"1"+"','"+"NULL"+"','"+subTopicId+"')");
 						insertDescIntoDb("insert into Law_Description1(law_description,country_id,sub_topic_id) Values('"+lawDescription+"','"+"1"+"','"+subTopicId+"')");
