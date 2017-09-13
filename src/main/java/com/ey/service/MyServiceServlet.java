4a02b0c8603c24907e213d6eab42f4f02afacbd1
saiba09 <saiba_sheikh@persistent.co.in>
2017-09-13 15:28:51 +0530

    added querries.java

--- src/main/java/com/ey/service/MyServiceServlet.java
+++ src/main/java/com/ey/service/MyServiceServlet.java
@@ -1,32 +1,25 @@
 package com.ey.service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import com.google.gson.JsonObject;
+
 
 import ai.api.AIServiceException;
 import ai.api.model.AIResponse;
 import ai.api.web.AIServiceServlet;
 
 public class MyServiceServlet extends AIServiceServlet {
+	private static final Logger log = Logger.getLogger(MyServiceServlet.class.getName());
 
 	private static final long serialVersionUID = 1L;
 	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		this.doPost(req, resp);
-	}
 	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -44,7 +37,7 @@ public class MyServiceServlet extends AIServiceServlet {
 			PrintWriter out = response.getWriter();
 			out.print(obj);	
 		} catch (AIServiceException e) {
-			System.out.println("Exception accesing API AI");
+			log.info("Exception accesing API AI");
 			e.printStackTrace();
 		}
 			
